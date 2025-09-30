d2ae25a4277de6a12175e66c4c1ac01464cba32f
Matthew Eiley <matthew.eiley@mail.mcgill.ca>
2025-09-30 12:21:57 -0400

    displays home (navigation)

--- allocor/src/App.jsx
+++ allocor/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Team from './components/Team'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Team/>
+      <Home/>
     </>
   )
 }
